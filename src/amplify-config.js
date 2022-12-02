22e81d8c1f3c7bfd3bbb55fa892beac992537207
voidm <marlkiller@voidm.com>
2022-12-02 20:16:51 +0800

    init

---
+++ src/amplify-config.js
@@ -0,0 +1,39 @@
+// This file is used for manual configuration of the Amplify library.
+// When Amplify is used in conjunction with the Amplify CLI toolchain or AWS Mobile Hub to manage backend resources,
+// an aws-exports.js file is auto-generated and can be used instead of the below to automatically configure the Amplify library.
+// In this workshop, we are using the Amplify client libraries without the CLI toolchain so you should edit this file manually.
+
+const awsConfig = {
+    Auth: {
+        identityPoolId: "us-east-2:15511981-349f-4406-94e0-802252d13798", // example: 'us-east-2:c85f3c18-05fd-4bb5-8fd1-e77e7627a99e'
+        region: "us-east-2", // example: 'us-east-2'
+        userPoolId: "us-east-2_RnKeGFdQ2", // example: 'us-east-2_teEUQbkUh'
+        userPoolWebClientId: "7rkhqabc1j9mls0nnabg26b0mp", // example: '3k09ptd8kn8qk2hpk07qopr86'
+
+        oauth: {
+            domain: "marlkiller.auth.us-east-2.amazoncognito.com",
+            scope: [
+                "phone",
+                "email",
+                "profile",
+                "openid",
+                "aws.cognito.signin.user.admin",
+            ],
+            redirectSignIn: "http://localhost:3000",
+            redirectSignOut: "http://localhost:3000",
+            responseType: "token", // or 'token', note that REFRESH token will only be generated when the responseType is code
+        },
+    },
+    API: {
+        endpoints: [
+            {
+                name: "WildRydesAPI",
+                endpoint:
+                    "https://de7es6k23g.execute-api.us-east-2.amazonaws.com/v1", // example: 'https://u8swuvl00f.execute-api.us-east-2.amazonaws.com/prod'
+                region: "us-east-2", // example: 'us-east-2'
+            },
+        ],
+    }
+};
+
+export default awsConfig;
---
+++ src/pages/HelloWorld.js
@@ -0,0 +1,86 @@
+import React, {useState} from "react";
+import {Auth, API} from "aws-amplify";
+import Amplify from "aws-amplify";
+import SiteNav from "../components/SiteNav";
+import SiteFooter from "../components/SiteFooter";
+
+const apiName = "WildRydesAPI";
+
+function HelloWorld(props) {
+
+    const [response, setResponse] = useState("Not Request");
+
+
+    const postNoAuth = async () => {
+        Amplify.Logger.LOG_LEVEL = "DEBUG";
+        const apiRequest = {
+            body: {},
+            headers: {
+                "Content-Type": "application/json",
+            },
+        };
+        return await API.post(apiName, "/no_auth", apiRequest);
+    };
+    const onClick_no_auth = async () => {
+        let resp = await postNoAuth();
+        console.log(resp);
+        setResponse(JSON.stringify(resp,null,2));
+    };
+
+    const postIAMAuth = async () => {
+        Amplify.Logger.LOG_LEVEL = "DEBUG";
+        const apiRequest = {
+            body: {},
+            headers: {
+                "Content-Type": "application/json",
+            },
+        };
+        return await API.post(apiName, "/iam_auth", apiRequest);
+    };
+    const onClick_iam_auth = async () => {
+        let resp = await postIAMAuth();
+        console.log(resp);
+        setResponse(JSON.stringify(resp,null,2));
+    };
+    const postCogAuth = async () => {
+        const session = await Auth.currentSession();
+        Amplify.Logger.LOG_LEVEL = "DEBUG";
+        const apiRequest = {
+            body: {},
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization":  session.getIdToken().getJwtToken()
+            },
+        };
+        return await API.post(apiName, "/cog_auth", apiRequest);
+    };
+    const onClick_cog_auth = async () => {
+        let resp = await postCogAuth();
+        console.log(resp);
+        setResponse(JSON.stringify(resp,null,2));
+    };
+
+    const divStyle = {
+        color: "#FFF",
+        background: "#000",
+        padding: "2.5rem 0",
+        textAlign: "center",
+    };
+    const RespStyle = {
+
+    };
+    return (
+        <div>
+            <SiteNav/>
+            <div style={divStyle} className="row column">
+                <button onClick={onClick_no_auth}>postNoAuth</button>
+                <button onClick={onClick_iam_auth}>postIAMAuth</button>
+                <button onClick={onClick_cog_auth}>postCogAuth</button>
+            </div>
+            <pre style={RespStyle}>{response}</pre>
+            <SiteFooter/>
+        </div>
+    );
+}
+
+export default HelloWorld;
